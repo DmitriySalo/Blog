6a4d6ac8e2fcf23659be559b9db645696b5bd418
DmitriySalo <dmitriyavanty@gmail.com>
2016-11-18 19:15:58 +0200

    Add image

--- Blog/blog/posts/models.py
+++ Blog/blog/posts/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+def upload_location(instance, filename):
+	# filebase,extension = filename.split(".")
+	# return "%s/%s.%s" %(instance.id,instance.id, extension)
+	return "%s/%s" %(instance.id, filename)
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	image = models.ImageField(	null=True, 
+								blank=True, 
+								upload_to = upload_location, 
+								height_field ="height_field", 
+								width_field="width_field")
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -13,3 +24,5 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"id":self.id})
 		# return "/posts/%s/" %(self.id)
+	class Meta(object):
+		ordering = ["-timestamp","-updated"]
--- Blog/blog/posts/views.py
+++ Blog/blog/posts/views.py
@@ -3,14 +3,27 @@ from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all()
+	paginator = Paginator(queryset_list, 5) # Show 25 queryset per page
+	page_request_var = 'post'
+	page = request.GET.get(page_request_var)
+	try:
+	    queryset = paginator.page(page)
+	except PageNotAnInteger:
+	    # If page is not an integer, deliver first page.
+	    queryset = paginator.page(1)
+	except EmptyPage:
+	    # If page is out of range (e.g. 9999), deliver last page of results.
+	    queryset = paginator.page(paginator.num_pages)
 	context = {
 		"object_list": queryset,
 		"title": "User list",
+		"page_request_var":page_request_var,
 
 	}
 
@@ -28,7 +41,7 @@ def post_list(request):
 	return render(request, 'blog/post_list.html',context)
 	#return HttpResponse("<h1>List</h1>")
 def post_create(request):
-	form =PostForm(request.POST or None)
+	form =PostForm(request.POST or None, request.FILES or None )
 	# if request.method == 'POST':
 	# 	print (request.POST.get("content"))
 	# 	print (request.POST.get("title"))
@@ -58,7 +71,7 @@ def post_detail(request,id):
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post,id=id)
-	form =PostForm(request.POST or None,instance=instance)
+	form =PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -79,3 +92,4 @@ def post_delete(request, id=None):
 	instance.delete()
 	messages.success(request,"Successfully deleted")
 	return redirect("posts:list")
+
